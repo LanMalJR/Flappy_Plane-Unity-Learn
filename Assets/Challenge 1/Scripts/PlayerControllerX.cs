c6ef08a96b29e9b1586e3858b960c49c3b6cea77
Langa LMJ <47083471+LanMalJR@users.noreply.github.com>
2025-04-04 14:03:52 +0200

    Update PlayerControllerX.cs
    Controls fixed now down is down and up is up.
    - For your own use look at line 27 and remove the (-) negative sign if the controls are inverted.

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public float speed = 10f;
-    public float rotationSpeed;
+    public float rotationSpeed = 50f;
     public float verticalInput;
 
     // Start is called before the first frame update
@@ -24,6 +24,6 @@ public class PlayerControllerX : MonoBehaviour
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * verticalInput * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * -verticalInput * Time.deltaTime);
     }
 }
